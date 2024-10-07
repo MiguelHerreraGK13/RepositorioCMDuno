05e217ae9425fdda857f45cace4b264dd0c78e32
MiguelHerreraGK13 <miguelherrera.gk1@gmail.com>
2024-10-07 12:29:30 -0500

    eventos, funciones, importanciones y exportaciones

---
+++ model/modelUsuarios.js
@@ -0,0 +1,13 @@
+export let usuario=[ {
+    usuario:"admin",
+    contrasena: '12345'
+},
+
+{
+    usuario: "superAdmin",
+    contrasena:'67890'
+}
+
+
+]
+// usuarios=objetos
---
+++ view/public/Js/script.js
@@ -0,0 +1,40 @@
+/*document.getElementById('')*/
+
+import { usuario } from "../../../model/modelUsuarios.js";
+
+console.log(usuario);
+
+
+function loggin(){   
+    let usuario = document.querySelector('#usuario').value
+    let contrasena = document.querySelector('#contraseña').value
+    if (usuario=='admin' && contrasena=='54321') {
+        console.log('esta correto!');
+        
+    }
+    else{
+        console.log('usuario o contraseña no coinciden');
+        
+    }
+    
+    
+    // console.log("iniciar Sesion");
+    // console.log(usuario);
+    // console.log(contrasena);
+    // console.log(loggin);
+    
+    
+    
+
+}
+
+/*funcion de espresion*/
+/*sintaxis*/
+/*let loggin=function(){
+    let usuario = document.querySelector('#usuario');
+    let contrasena= document.querySelector('#contraseña');
+
+    console.log("iniciar Sesion");
+}*/
+
+
