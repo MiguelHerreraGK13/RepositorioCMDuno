34afbbba2d6c9da08345b6b7d1bd6d7556a72fbe
MiguelHerreraGK13 <miguelherrera.gk1@gmail.com>
2024-10-28 12:25:32 -0500

    manupulacion del DOM con pagina home, botones de filtrado y arreglar codigo que ya no funciona

---
+++ controller/controllerEvo.js
@@ -0,0 +1,47 @@
+import { recetasVegetarianas } from "../model/modelRecetas.js";
+import { recetasNoVegetarianas } from "../model/modelRecetas.js";
+
+
+
+export function listarRecetasVegetarianas() {
+    document.getElementById('contenido').innerHTML=" "
+
+    recetasVegetarianas.map((recetavegetariana)=>{
+        let contenedor=document.createElement('div')
+        let nombre=document.createElement('p')
+        let ingredientes=document.createElement('ul')
+        let tiempoPreparacion=document.createElement('p')
+        let dificultad=document.createElement('p')
+        let tipoComida=document.createElement('p')
+
+        nombre.textContent= "nombre"+ recetasVegetarianas.nombre
+        ingredientes.textContent= "ingredientes" + recetasVegetarianas.ingredientes
+        tiempoPreparacion.textContent= "tiempoPreparacion" + recetasVegetarianas.tiempoPreparacion
+        dificultad.textContent= "dificultad" +recetasVegetarianas.dificultad
+        tipoComida.textContent= "tipoComida" + recetasVegetarianas.tipoComida
+        
+        contenedor.append(nombre,ingredientes,tiempoPreparacion,dificultad,tipoComida)
+        document.getElementById('contenido').append(contenedor)
+
+    }) //generalmente el parametro interno de la flecha en singular del arreglo ppal
+}
+export function listarRecetasNoVegetarianas() {
+    recetasNoVegetarianas.map((recetaNoVegetariana)=>{
+        let contenedor=document.createElement('div')
+        let nombre=document.createElement('p')
+        let ingredientes=document.createElement('ul')
+        let tiempoPreparacion=document.createElement('p')
+        let dificultad=document.createElement('p')
+        let tipoComida=document.createElement('p')
+
+        contenedor.append(nombre,ingredientes,tiempoPreparacion,dificultad,tipoComida)
+        document.getElementById('contenido').append(contenedor)
+
+        nombre.textContent= "nombre"+ recetasNoVegetarianas.nombre
+        ingredientes.textContent= "ingredientes" + recetasNoVegetarianas.ingredientes
+        tiempoPreparacion.textContent= "tiempoPreparacion" + recetasNoVegetarianas.tiempoPreparacion
+        dificultad.textContent= "dificultad" +recetasNoVegetarianas.dificultad
+        tipoComida.textContent= "tipoComida" + recetasNoVegetarianas.tipoComida
+})
+}
+
--- view/public/Js/script.js
+++ view/public/Js/script.js
@@ -1,15 +1,35 @@
 /*document.getElementById('')*/
 
 import { buscarUsuario } from "/controller/controllerUsuario.js";
+import { listarRecetasNoVegetarianas, listarRecetasVegetarianas } from "../../../controller/controllerEvo.js";
 
-document.getElementById('btnLoggin').addEventListener('click',loggin)
+
+let btnIniciar=document.getElementById('btnLoggin')
+
+if (btnIniciar) {
+    document.getElementById('btnLoggin').addEventListener('click',loggin)
     function loggin(){ 
         if (buscarUsuario()) {
             window.location.href = '/view/page/home.html'  //redireccion a home con ruta absoluta o relativa, depende del servidor
         }else{
             console.log("error de credenciales");            
         }       
-    }
+    }    
+}
+
+document.getElementById('evo1').addEventListener('click',()=>{
+    
+    listarRecetasVegetarianas()
+})
+document.getElementById('evo2').addEventListener('click',()=>{
+    
+    listarRecetasNoVegetarianas()
+})
+document.getElementById('cerrarSesion').addEventListener('click',()=>{
+    Window.location.href= '/index.html'
+})
+
+
     
     // console.log("iniciar Sesion");
     // console.log(usuario);
